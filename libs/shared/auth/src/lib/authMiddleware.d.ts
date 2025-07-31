f938b7c26972bf57b19ef32b9f030bb3cc4db03e
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2025-07-31 17:20:41 +0530

    Add files via upload

---
+++ libs/shared/auth/src/lib/authMiddleware.d.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import { AuthPayload, UserRole } from './types';
+declare module 'express' {
+    interface Request {
+        user?: AuthPayload;
+    }
+}
+export declare function authMiddleware(allowedRoles?: UserRole | UserRole[], secret?: string): (req: Request, res: Response, next: NextFunction) => void;
+export declare const requireAuth: typeof authMiddleware;
+//# sourceMappingURL=authMiddleware.d.ts.map
