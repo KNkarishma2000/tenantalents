de7151f028100431b513fbabe11b119be5d1867e
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2025-07-31 17:18:47 +0530

    Add files via upload

---
+++ libs/shared/error/src/lib/not-found-handler.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.notFoundHandler = void 0;
+const api_error_1 = require("./api-error");
+const notFoundHandler = (req, _res, next) => {
+    const message = `🔍 Not Found - ${req.originalUrl}`;
+    next(new api_error_1.ApiError(404, message));
+};
+exports.notFoundHandler = notFoundHandler;
+//# sourceMappingURL=not-found-handler.js.map
